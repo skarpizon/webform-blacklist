4691e95c9ab66fbb91ead7071282721bc5ce1529
Anton Skulkin <skarpizon@mail.ru>
2019-11-18 11:11:45 +0300

    Updates

---
+++ precache-manifest.a2f05a1a07db88488bf2365252c6054b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c0cae59b5cc9401175da9717764031a8",
+    "url": "/webform-blacklist/index.html"
+  },
+  {
+    "revision": "307298d02fe0b2fcf7b6",
+    "url": "/webform-blacklist/static/css/main.ff0a3c61.chunk.css"
+  },
+  {
+    "revision": "c0f06ca91a00910d77b6",
+    "url": "/webform-blacklist/static/js/2.0fd87874.chunk.js"
+  },
+  {
+    "revision": "307298d02fe0b2fcf7b6",
+    "url": "/webform-blacklist/static/js/main.abe913a5.chunk.js"
+  },
+  {
+    "revision": "8657ce61bb83b8765e94",
+    "url": "/webform-blacklist/static/js/runtime~main.12e14963.js"
+  }
+]);
